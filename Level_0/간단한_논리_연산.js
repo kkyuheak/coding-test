69552fc6e90de4f89da6183d70e29c182221b273
kkyuheak <gyuheak@gmail.com>
2024-07-04 22:32:44 +0900

    PGS 레벨0 간단한_논리_연산

---
+++ Level_0/간단한_논리_연산.js
@@ -0,0 +1,13 @@
+// 내 풀이
+function solution(x1, x2, x3, x4) {
+  let answer = true;
+  answer = (x1 || x2) && (x3 || x4);
+
+  return answer;
+}
+
+// 다른 사람 풀이
+
+function solution(x1, x2, x3, x4) {
+  return (x1 || x2) && (x3 || x4);
+}
