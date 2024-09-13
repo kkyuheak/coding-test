d0ab7587ea7f4678de861036ad2be7a4300181ba
kkyuheak <gyuheak@gmail.com>
2024-09-13 14:40:58 +0900

    baekjoon 문제풀이 처음부터 시작

---
+++ baekjoon/입출력문제/A+B.js
@@ -0,0 +1,10 @@
+// 문제 https://www.acmicpc.net/problem/1000
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let line = input[0].split(" ");
+
+let a = parseInt(line[0]);
+let b = parseInt(line[1]);
+
+console.log(a + b);
