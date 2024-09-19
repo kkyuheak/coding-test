420610d45a94947c8e2639a33fe462fe59cc39a2
kkyuheak <gyuheak@gmail.com>
2024-09-19 21:18:11 +0900

    bj 10818번 최대값, 최솟값

---
+++ baekjoon/배열문제/최대값최솟값.js
@@ -0,0 +1,7 @@
+// https://www.acmicpc.net/problem/10818
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+const arr = input[1].split(" ");
+
+console.log(Math.min(...arr), Math.max(...arr));
